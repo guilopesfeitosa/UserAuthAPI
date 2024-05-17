f491edb5c7593f898282c91a11984bb86613dd1b
guilopesfeitosa <guilopesfeitosa@gmail.com>
2024-05-17 15:40:06 -0300

    feat: remove dependencias de Animals

--- src/main/java/com/example/fullstackloginbackend/domain/animals/AnimalsRequestDto.java
+++
@@ -1,3 +0,0 @@
-package com.example.fullstackloginbackend.domain.animals;
-
-public record AnimalsRequestDto(String name, String description) {}
--- src/main/java/com/example/fullstackloginbackend/repositories/AnimalsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.fullstackloginbackend.repositories;
-
-import com.example.fullstackloginbackend.domain.animals.Animal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AnimalsRepository extends JpaRepository<Animal, UUID> {}
